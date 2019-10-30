cd0b3f3f461e90fd5ec4846ee47c347dcc3bf8de
kuranai <mail@kuranai.de>
2019-10-30 14:28:55 +0100

    added emoji fonts

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -2,5 +2,6 @@ pacman -S xclip
 pacman -S go
 pacman -S nodejs
 pacman -S ripgrep
+pacman -S ttf-liberation noto-fonts noto-fonts-emoji #install Emoji-Fonts
 yay -S visual-studio-code-bin
 yay -S python-pipx
